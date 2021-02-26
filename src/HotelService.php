cfbe611a0e66c3a8ef17ae54c9085498a3747f4a
Jovi <jovi.anto@misteraladin.com>
2021-02-26 17:12:04 +0700

    [Hotel] update hotel service. Add create booking, add cancel booking

--- src/HotelService.php
+++ src/HotelService.php
@@ -4,8 +4,13 @@ namespace Redzjovi\ExpediaPhp;
 
 class HotelService
 {
-    public function booking() : string
+    public function createBooking() : string
     {
         return 'Create booking successfull';
+    }
+
+    public function cancelBooking() : string
+    {
+        return 'Booking has been cancelled';
     }
 }
